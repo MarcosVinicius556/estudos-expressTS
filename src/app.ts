c2d9e137ab9b4cffda9acbe09241a0b269a199d4
MarcosVinicius556 <marcosvinicios4132@gmail.com>
2024-07-02 10:08:56 -0300

    Aplicação inicial configurada, primeira rota criada

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,23 @@
 //Iniciando projeto
+import express from 'express';
 
-console.log("Express + TS")
+/**
+ * Incializando a aplicação EXPRESS
+ */
+const app = express();
+
+/**
+ * Aqui estamos definindo uma rota, que por padrão no express, sempre virá com 2 parâmetros,
+ * onde uma será a "req" de onde tiramos valores vindos da requisição, e o outro o "res" que seria nossa resposta (caso seja necessário devolver algo)
+ * 
+ */
+app.get('/', (req, res) => {
+    return res.send("HELLO EXPRESS!");
+})
+
+/**
+ * Definindo a porta em que a aplicação estará rodando, e também, ao iniciar, ele irá exibir esta mensagem
+ */
+app.listen(3000, () => {
+    console.log('Aplicação Express + TS rodando')
+})
